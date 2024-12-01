043c1f1be682fbb7ad2a7908c4a69bd3d4086646
Adam CRIPPS <Adam.Cripps@saint-gobain.com>
2024-12-01 19:46:19 +0000

    Clean output

--- day01.py
+++ day01.py
@@ -42,21 +42,20 @@ def count_list_frequency(left_list, right_list):
 
 if __name__ == '__main__':
     left, right = separate_nums('1-input.txt')
-    print(f"Original Left list: {left}")
-    print(f"Original Right list: {right}")
+    # print(f"Original Left list: {left}")
+    # print(f"Original Right list: {right}")
 
     left, right = sort_lists(left, right)
-    print(f"Sorted Left list: {left}")
-    print(f"Sorted Right list: {right}")
+    # print(f"Sorted Left list: {left}")
+    # print(f"Sorted Right list: {right}")
 
     differences = compare_lists(left, right)
-    print(f"Differences between lists: {differences}")
+    # print(f"Differences between lists: {differences}")
 
     total_difference = sum(differences)
     print(f"Total difference: {total_difference}")
 
-    frequency_result = count_list_frequency(left, right)
-    print(f"Frequency result: {frequency_result}")
 
+    frequency_result = count_list_frequency(left, right)
     total_frequency = sum(frequency_result)
     print(f"Total frequency: {total_frequency}")
